28142976a20480755ec3a50130a93efe45dea111
John Ault <john.ault.dev@gmail.com>
2023-08-08 22:02:48 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import avatar from './assets/images/sdDG-generic.png'; // Tell webpack this JS file uses this image
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <img src={avatar} alt="avatar" height='250' />;
+      <h1>John Smith</h1>
+      <h3>Software Engineer</h3>
+      <div className="borderDiv" style={{border: 1 + 'px solid black', padding: 20 + 'px', width: 600 + 'px', margin: 'auto', marginBottom: 20 + 'px'}}>
+        <strong>About me</strong><br></br>
+        <hr></hr>
+        I am an advocate of self-improvement and I want to help people improve their lives through knowledge and application
+      </div>
+
+      <div className="borderDiv" style={{border: 1 + 'px solid black', padding: 20 + 'px', width: 600 + 'px', margin: 'auto', marginBottom: 20 + 'px'}}>
+        <strong>Contact me</strong><br></br>
+        <hr></hr>
+        Email: <a href='mailto:sample_john_smith@mail.com'>sample_john_smith@mail.com</a> | LinkedIn: <a href="https://www.linkedin.com/">https://www.linkedin.com/</a>
+
+
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
